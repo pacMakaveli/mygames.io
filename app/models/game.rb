d18d1ec6f36706937e31cfc33e08a65da19a9a6a
Vlad Radulescu <pacMakaveli90@gmail.co.uk>
2015-08-16 00:51:29 +0100

    a user has many games and one can now quickly add a gome to his collection

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,8 @@
 class Game < ActiveRecord::Base
 
+
+  # validates_uniqueness_of :reference_id, scope: :user_id
+
   def self.search(game)
     @search = GiantBomb::Search.new
     # @search.limit(10)
@@ -9,5 +12,25 @@ class Game < ActiveRecord::Base
 
 
     return @search.fetch
+  end
+
+  # Creates a local copy of the Games based on it's ID from GiantBomb' API
+  #
+  def self.new_from_gb(id)
+    data = {
+      field_list: 'name'
+    }
+
+    game = GiantBomb::Game.detail(id, data)
+
+    g = Game.new
+    g.name = game.name
+    g.description = game.deck
+    g.reference_id = id
+    g.user_id = User.first
+    g.save
+
+    Rails.logger.info game.deck
+    # Rails.logger.info game['deck'].inspect
   end
 end
