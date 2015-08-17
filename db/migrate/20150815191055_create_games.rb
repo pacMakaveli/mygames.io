f72d7b82ccb2cb409e55f225308cb9748bf0b79f
Vlad Radulescu <pacMakaveli90@gmail.co.uk>
2015-08-17 20:47:58 +0100

    redo the schema and migrations

--- db/migrate/20150815191055_create_games.rb
+++ db/migrate/20150815191055_create_games.rb
@@ -1,17 +1,25 @@
 class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
-      t.belongs_to :user,       index: true, foreign_key: true
+      t.belongs_to :platform,  foreign_key: true
 
-      t.string :name,           null: false
-      t.text :description,      null: false
-      t.string :platform
+      t.string :name,          null: false
+      t.string :aliases
+      t.text :description,     null: false
       t.attachment :cover
 
+      t.string :platforms
       t.string :endpoint
-      t.integer :reference_id,  index: true, null: false
+      t.integer :reference_id, null: false
 
+      t.date :release_date
       t.timestamps null: false
     end
+
+    add_index :games, :name
+    add_index :games, :reference_id
+    add_index :games, :endpoint
+
+    add_index :games, :platform_id
   end
 end
--- db/migrate/20150816124616_create_wikis.rb
+++ db/migrate/20150816124616_create_wikis.rb
@@ -1,15 +1,17 @@
 class CreateWikis < ActiveRecord::Migration
   def change
     create_table :wikis do |t|
-      t.belongs_to :game, index: true, foreign_key: true
+      t.belongs_to :game, foreign_key: true
 
-      t.string :genre
       t.text :body
-      t.string :theme
 
-      t.date :release_date
+      t.string :theme
+      t.string :developer
+      t.string :publisher
 
       t.timestamps null: false
     end
+
+    add_index :wikis, :game_id
   end
 end
